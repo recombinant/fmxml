39a737a128520044d897e9c7b1bff38b772d6b6a
recombinant <gituser@recombinant.me.uk>
2016-11-11 15:55:52 +0000

    FileMakerServer now correctly exported from fmxml python library Python 3.5 only

--- fmxml/__init__.py
+++ fmxml/__init__.py
@@ -5,3 +5,8 @@ try:
     __version__ = pkg_resources.get_distribution(__name__).version
 except:
     __version__ = 'unknown'
+
+from .fms import FileMakerServer
+from . import commands
+from . import parsers
+from . import structure
